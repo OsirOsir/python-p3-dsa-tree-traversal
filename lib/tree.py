bd2b5c035e43d88772cd8b1c6835d4c0eef595f5
Philip osir Otieno <philiposir2@gmail.com>
2024-09-15 15:07:05 +0300

    update

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,31 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    def dfs(node):
+      if node['id'] == id:
+        return node
+      
+      for child in node['children']:
+        result = dfs(child)
+        if result:
+          return result
+        
+      return None
+    
+    return dfs(self.root)
+  
+  def get_element_by_id_bfs(self, id):
+    queue = [self.root]
+    
+    while queue:
+      node = self.root.pop(0)
+      
+      if node['id'] == id:
+        return node
+      
+      queue.extend(node['children'])
+    
+    return None
+    
+  
+  
